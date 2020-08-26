23a10a347cdd86b79814c2f907a0f15415075897
chzgustavo <g.choquevilca@unc.edu.ar>
2020-08-26 15:43:46 -0300

    Modifique algunas funciones y agregue a todos los métodos atributo error

--- README.md
+++ README.md
@@ -40,7 +40,7 @@ Las dependencias con las que cuenta Lift0ff son:
 - [x] List Of Open File **lsof**, necesario para mostrar los puertos de Pc. Verificar binario instalado: ***which lsof***
 - [x] Directorio **/proc** para recopilar datos
 - [x] Instalar paquetes de Go para mostrar tablas: ***go get github.com/rodaine/table*** y ***go get github.com/fatih/color***
-- [x] Instalar paquetes de Go para mostrar porcentaje de cpu: ***go get github.com/shirou/gopsutil/cpu***. Consultar al repositorio: **[gopsutil](https://github.com/shirou/gopsutil)** 
+- [x] Instalar paquete de Go para mostrar porcentaje de cpu: ***go get github.com/shirou/gopsutil/cpu***. Consultar al repositorio: **[gopsutil](https://github.com/shirou/gopsutil)** 
  
 
 ## Instalación y Ejecución
@@ -48,7 +48,7 @@ Las dependencias con las que cuenta Lift0ff son:
 Instale Go si aún no lo ha hecho.
 Descargue el repositorio y en el directorio source ejecute:
 
-```go build Lift0ff.go```
+```go build lift0ff.go```
 
 ### Ejecución aplicación:
 
--- source/lift0ff.go
+++ source/lift0ff.go
@@ -18,83 +18,101 @@ import (
 	"github.com/shirou/gopsutil/cpu"
 )
 
-func execComand(com *exec.Cmd) {
-	c := com
+func execComand(comando *exec.Cmd) error {
+	c := comando
 	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	err := c.Run()
-
 	if err != nil {
-		fmt.Println("Error execution: ", err)
-		fmt.Println("Error")
-	} /*else {
-		fmt.Println("Ejecucion correcta")
-	}*/
+		return err
+	}
+	return nil
 }
 
-func singleData(path string) string {
+func hostname() (string, error) {
 	st := ""
-	file, err := os.Open(path)
+	file, err := os.Open("/proc/sys/kernel/hostname")
 	if err != nil {
-		//log.Fatal(err)
-		fmt.Println(err)
+		return "", err
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		st = scanner.Text()
 	}
-	return st
+	return st, nil
 }
 
-func simpleData(path string, dato string) string {
-	st := ""
-	file, err := os.Open(path)
+func rtcInfo(dato string) (string, error) {
+	rtcInfo := ""
+	file, err := os.Open("/proc/driver/rtc")
 	if err != nil {
-		//log.Fatal(err)
-		fmt.Println(err)
+		return "", err
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		if strings.Contains(scanner.Text(), dato) {
-			st = scanner.Text()
-			//st = removeSpace(st)
-			noSpaceString := strings.ReplaceAll(st, "\t", "")
-			st = strings.TrimPrefix(noSpaceString, dato+": ")
+			_, err := fmt.Sscanf(scanner.Text(), dato+" : %s", &rtcInfo)
+			if err != nil {
+				return "", err
+			}
 			break
 		}
 	}
-	return st
+	return rtcInfo, nil
 }
 
-func kernelVersion(path string) (string, string) {
-	kv1, kv2 := "", ""
-	file, err := os.Open(path)
+func cpuInfo(dato string) (string, error) {
+	sysInfo := ""
+	file, err := os.Open("/proc/cpuinfo")
 	if err != nil {
-		//log.Fatal(err)
-		fmt.Println(err)
+		return "", err
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		_, err := fmt.Sscanf(scanner.Text(), "%s version %s", &kv1, &kv2)
+		if strings.Contains(scanner.Text(), dato) {
+			if dato == "model name" {
+				noSpaceString := strings.ReplaceAll(scanner.Text(), "\t", "")
+				sysInfo = strings.TrimPrefix(noSpaceString, dato+": ")
+				break
+			}
+			_, err := fmt.Sscanf(scanner.Text(), dato+" : %s", &sysInfo)
+			if err != nil {
+				return "", err
+			}
+			break
+		}
+	}
+	return sysInfo, nil
+}
+
+func kernelVersion() (string, error) {
+	kv := ""
+	file, err := os.Open("/proc/version")
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		_, err := fmt.Sscanf(scanner.Text(), "Linux version %s", &kv)
 		if err != nil {
-			//panic(err)
-			fmt.Println(err)
+			return "", err
 		}
 		break
 	}
-	return kv1, kv2
+	return kv, nil
 }
 
-func versionSo(path string, dato string) string {
+func versionSo() (string, error) {
 	st := ""
-	file, err := os.Open(path)
+	dato := "PRETTY_NAME"
+	file, err := os.Open("/etc/os-release")
 	if err != nil {
-		//log.Fatal(err)
-		fmt.Println(err)
+		return "", err
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
@@ -106,96 +124,92 @@ func versionSo(path string, dato string) string {
 			break
 		}
 	}
-	return st
+	return st, err
 }
 
-func tiempoActivoSo(path string) float64 {
-	var tA float64 = 0.0
-
-	file, err := os.Open(path)
+func uptime() (float64, error) {
+	var up float64 = 0
+	file, err := os.Open("/proc/uptime")
 	if err != nil {
-		//log.Fatal(err)
-		fmt.Println(err)
+		return 0, err
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-
 	for scanner.Scan() {
-		_, err := fmt.Sscanf(scanner.Text(), "%f", &tA)
+		_, err := fmt.Sscanf(scanner.Text(), "%f", &up)
 		if err != nil {
-			panic(err)
+			return 0, err
 		}
 		break
 	}
-	return tA
+	return up, nil
 }
 
-func fechaInicioSistema(com *exec.Cmd) (string, string) {
+func who() (string, error) {
 	f1, f2 := "", ""
-	out, err := com.Output()
+	out, err := exec.Command("who", "-b").Output()
 	if err != nil {
-		//log.Fatal(err)
-		fmt.Println(err)
+		return "", err
 	}
 	_, error := fmt.Sscanf(string(out), " arranque del sistema %s %s", &f1, &f2)
 	if error != nil {
-		//panic(error)
-		fmt.Println(error)
+		return "", error
 	}
-	return f1, f2
+	return f1 + " " + f2, nil
 }
 
-func memInfo(mem string) float64 {
-	var m1 float64
+func memInfo(mem string) (float64, error) {
+	var m float64
 	file, err := os.Open("/proc/meminfo")
 	if err != nil {
-		fmt.Println(err)
+		return 0, err
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		if strings.Contains(scanner.Text(), mem) {
-			_, err := fmt.Sscanf(scanner.Text(), mem+" %f", &m1)
+			_, err := fmt.Sscanf(scanner.Text(), mem+" %f", &m)
 			if err != nil {
-				fmt.Println(err)
+				return 0, err
 			}
 			break
 		}
 	}
-	return m1
+	return m, nil
 }
 
-func memDisk() (string, string, string, string) {
+func memDisk() ([]string, error) {
 	m1, m2, m3, m4, m5, m6 := "", "", "", "", "", ""
+	mDisk := make([]string, 0)
 	cmd := exec.Command("df", "-h")
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		fmt.Println(err)
+		return mDisk, err
 	}
 	if err := cmd.Start(); err != nil {
-		fmt.Println(err)
+		return mDisk, err
 	}
 	scanner := bufio.NewScanner(stdout)
 	for scanner.Scan() {
 		_, error := fmt.Sscanf(scanner.Text(), "%s %s %s %s %s %s", &m1, &m2, &m3, &m4, &m5, &m6)
 		if error != nil {
-			//panic(error)
-			fmt.Println(error)
+			return mDisk, err
 		}
 		if m6 == "/" {
+			mDisk = append(mDisk, m2, m3, m4, m5)
 			break
 		}
 	}
-	return m2, m3, m4, m5
+	return mDisk, nil
 }
 
-func networkData() *list.List {
+func networkData() (*list.List, error) {
 	n1, n2, n3, n4, n5, n6, n7, n8 := "", "", "", "", "", "", "", ""
 	listaRed := list.New()
 	red := make([]string, 9)
 	file, err := os.Open("/proc/net/dev")
 	if err != nil {
-		fmt.Println(err)
+		return listaRed, err
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
@@ -217,18 +231,18 @@ func networkData() *list.List {
 
 			_, er1 := fmt.Sscanf(downl, "%s %s %s %s", &n1, &n2, &n3, &n4)
 			if er1 != nil {
-				fmt.Println(er1)
+				return listaRed, er1
 			}
 			_, er2 := fmt.Sscanf(upl, "%s %s %s %s", &n5, &n6, &n7, &n8)
 			if er2 != nil {
-				fmt.Println(er2)
+				return listaRed, er2
 			}
 			red[0], red[1], red[2], red[3], red[4], red[5], red[6], red[7], red[8] = intfz, n1, n2, n3, n4, n5, n6, n7, n8
 			listaRed.PushBack(red)
 			red = []string{"", "", "", "", "", "", "", "", ""}
 		}
 	}
-	return listaRed
+	return listaRed, nil
 }
 
 func help() {
@@ -247,21 +261,62 @@ func help() {
 }
 func info() {
 	fmt.Println("\nInformacion del Sistema\n")
-	fmt.Println("       Fecha y Hora RTC:", simpleData("/proc/driver/rtc", "rtc_date"), simpleData("/proc/driver/rtc", "rtc_time"))
-	fmt.Println("	       Hostname:", singleData("/proc/sys/kernel/hostname"))
-	fmt.Println("  Fabricante Procesador:", simpleData("/proc/cpuinfo", "vendor_id"))
-	fmt.Println("      Modelo Procesador:", simpleData("/proc/cpuinfo", "model name"))
-	kv1, kv2 := kernelVersion("/proc/version")
-	fmt.Println("      Vesion del kernel:", kv1, kv2)
-	fmt.Println("	  Version de SO:", versionSo("/etc/os-release", "PRETTY_NAME"))
-	tA := tiempoActivoSo("/proc/uptime")
-	dias := int(tA / 86400)
-	horas := int((tA) / 3600)
-	minutos := int(((tA / 3600) - float64(horas)) * 60)
-	segundos := int(((((tA / 3600) - float64(horas)) * 60) - float64(minutos)) * 60)
+
+	fecha, err1 := rtcInfo("rtc_date")
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	hora, err2 := rtcInfo("rtc_time")
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	fmt.Println("       Fecha y Hora RTC:", fecha, hora)
+
+	hostname, err3 := hostname()
+	if err3 != nil {
+		fmt.Println(err3)
+	}
+	fmt.Println("	       Hostname:", hostname)
+
+	vendor, err4 := cpuInfo("vendor_id")
+	if err4 != nil {
+		fmt.Println(err4)
+	}
+	fmt.Println("  Fabricante Procesador:", vendor)
+
+	modelo, err5 := cpuInfo("model name")
+	if err5 != nil {
+		fmt.Println(err5)
+	}
+	fmt.Println("      Modelo Procesador:", modelo)
+
+	kv, err6 := kernelVersion()
+	if err6 != nil {
+		fmt.Println(err6)
+	}
+	fmt.Println("      Vesion del kernel:", kv)
+
+	vs, err7 := versionSo()
+	if err7 != nil {
+		fmt.Println(err7)
+	}
+	fmt.Println("	  Version de SO:", vs)
+
+	up, err8 := uptime()
+	if err8 != nil {
+		fmt.Println(err8)
+	}
+	dias := int(up / 86400)
+	horas := int((up) / 3600)
+	minutos := int(((up / 3600) - float64(horas)) * 60)
+	segundos := int(((((up / 3600) - float64(horas)) * 60) - float64(minutos)) * 60)
 	fmt.Printf("       Tiempo activo So: %vd :%vh :%vm :%vs \n", dias, horas, minutos, segundos)
-	f1, f2 := fechaInicioSistema(exec.Command("who", "-b"))
-	fmt.Printf("	 Inicio sistema: %s %s\n\n", f1, f2)
+
+	start, err9 := who()
+	if err9 != nil {
+		fmt.Println(err9)
+	}
+	fmt.Printf("	 Inicio sistema: %s \n\n", start)
 }
 
 func version() {
@@ -276,28 +331,35 @@ func version() {
 }
 
 func mem() {
-	m1, m2, m3, m4 := memDisk()
-	fmt.Println("\nMemoria Disco\n")
-	fmt.Println("Memory:")
-	fmt.Println("    Memory Total:", m1)
-	fmt.Println("     Memory Used:", m2)
-	fmt.Println("Memory Available:", m3)
-	fmt.Printf(" percentage used: %s\n\n", m4)
+	disk, err := memDisk()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("\nMemoria Disco\n")
+		fmt.Println("Memory:")
+		fmt.Println("    Memory Total:", strings.Join(disk[0:1], ""))
+		fmt.Println("     Memory Used:", strings.Join(disk[1:2], ""))
+		fmt.Println("Memory Available:", strings.Join(disk[2:3], ""))
+		fmt.Printf(" percentage used: %s\n\n", strings.Join(disk[3:4], ""))
+	}
 }
 
 func port() {
-	execComand(exec.Command("sudo", "lsof", "-i", "-P"))
+	err := execComand(exec.Command("sudo", "lsof", "-i", "-P"))
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println()
 }
 
 func processor() {
 	fmt.Println()
 	for i := 0; i < 5; i++ {
-		headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
-		columnFmt := color.New(color.FgHiRed).SprintfFunc()
+		titulo := color.New(color.FgGreen, color.Underline).SprintfFunc()
+		columna := color.New(color.FgHiRed).SprintfFunc()
 
 		tblp := table.New("CPU", "Porcentaje")
-		tblp.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+		tblp.WithHeaderFormatter(titulo).WithFirstColumnFormatter(columna)
 		pCpu, _ := cpu.Percent(time.Second, false)
 		pCore, _ := cpu.Percent(1500*time.Millisecond, true)
 		tblp.AddRow("Cpu-Total", fmt.Sprintf("%.2f %%", pCpu[0]))
@@ -310,28 +372,36 @@ func processor() {
 }
 
 func proct() {
-	execComand(exec.Command("top", "-d", "5"))
+	err := execComand(exec.Command("top", "-d", "5"))
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println()
 }
 
 func proce() {
-	execComand(exec.Command("ps", "aux"))
+	err := execComand(exec.Command("ps", "aux"))
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println()
 }
 
 func ram() {
 	fmt.Println("\nMemoria RAM\n")
 	fmt.Println("Memory:")
-	memTotal := memInfo("MemTotal:")
-	memAvailable := memInfo("MemAvailable:")
+	memTotal, _ := memInfo("MemTotal:")
+	memAvailable, _ := memInfo("MemAvailable:")
 	fmt.Println("    Memory Total:", math.Round(memTotal*0.000001*100)/100, "GiB")
 	fmt.Println("     Memory Used:", math.Round((memTotal-memAvailable)*0.000001*100)/100, "GiB")
 	fmt.Println("Memory Available:", math.Round(memAvailable*0.001*100)/100, "MiB")
-	fmt.Println("    Memory Cache:", math.Round(memInfo("Cached:")*0.001*100)/100, "MiB")
-	fmt.Println("     Memory Free:", math.Round(memInfo("MemFree:")*0.001*100)/100, "MiB")
+	cache, _ := memInfo("Cached:")
+	memFree, _ := memInfo("MemFree:")
+	fmt.Println("    Memory Cache:", math.Round(cache*0.001*100)/100, "MiB")
+	fmt.Println("     Memory Free:", math.Round(memFree*0.001*100)/100, "MiB")
 	fmt.Println("\nMemory Swap:")
-	memTotalSwap := memInfo("SwapTotal:")
-	memSwapFree := memInfo("SwapFree:")
+	memTotalSwap, _ := memInfo("SwapTotal:")
+	memSwapFree, _ := memInfo("SwapFree:")
 	fmt.Println("     Total Swap:", math.Round(memTotalSwap*0.000001*100)/100, "GiB")
 	fmt.Println("      Swap Used:", math.Round((memTotalSwap-memSwapFree)*0.000001*100)/100, "GiB")
 	fmt.Println("      Swap Free:", math.Round(memSwapFree*0.000001*100)/100, "GiB", "\n")
@@ -341,16 +411,19 @@ func red() {
 	fmt.Println("\nDatos de Red\n")
 	fmt.Println("Datos Recibidos:")
 
-	headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
-	columnFmt := color.New(color.FgHiRed).SprintfFunc()
+	titulo := color.New(color.FgGreen, color.Underline).SprintfFunc()
+	columna := color.New(color.FgHiRed).SprintfFunc()
 
 	tbld := table.New("Interface", "bytes", "packets", "errs", "drop")
-	tbld.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+	tbld.WithHeaderFormatter(titulo).WithFirstColumnFormatter(columna)
 
 	tblu := table.New("Interface", "bytes", "packets", "errs", "drop")
-	tblu.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+	tblu.WithHeaderFormatter(titulo).WithFirstColumnFormatter(columna)
 
-	listaRed := networkData()
+	listaRed, err := networkData()
+	if err != nil {
+		fmt.Println(err)
+	}
 	for temp := listaRed.Front(); temp != nil; temp = temp.Next() {
 		s := reflect.ValueOf(temp.Value)
 		tbld.AddRow(s.Index(0), s.Index(1), s.Index(2), s.Index(3), s.Index(4))
@@ -365,7 +438,7 @@ func red() {
 func main() {
 	argsConProg := os.Args
 	if len(argsConProg) == 1 {
-		fmt.Println("Error, ejecute  liftoff --help, para ver comando habilitados")
+		fmt.Println("Error, ejecute  ./liftoff --help, para ver comando habilitados")
 		os.Exit(1)
 	}
 	sentencia := os.Args[1:]
